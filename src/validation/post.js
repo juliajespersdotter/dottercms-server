bafe39057ac72902d43a80763d5e61d8898690f1
Julia Jespersdotter <julia.jespersdotter@gmail.com>
2022-11-03 11:00:50 +0100

    chedking so datetime exists in validation rules.

--- src/validation/post.js
+++ src/validation/post.js
@@ -7,13 +7,13 @@ const { body } = require('express-validator')
 const createRules = [
 	body('title').exists().isLength({ min: 3 }).trim(),
 	body('content').exists().isLength({ min: 3 }).trim(),
-	body('created_at'),
+	body('created_at').exists(),
 ]
 
 const updateRules = [
 	body('title').exists().isLength({ min: 3 }).trim(),
 	body('content').exists().isLength({ min: 3 }).trim(),
-	body('created_at'),
+	body('created_at').exists(),
 ]
 
 module.exports = {
